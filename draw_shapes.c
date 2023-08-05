ca516cb361369837d7f804d35c3547f604f0862a
Javier Ontiveros <50270099+jontiveros1515@users.noreply.github.com>
2023-08-05 04:18:26 -0600

    Adding changes

---
+++ draw_shapes.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include "draw.h"
+
+/* Prints a size x size square whose left col is at startCol */
+void print_square(int leftCol, int size)
+{
+  int i, j;
+  int endCol = leftCol + size;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol; col++) putchar(' ');
+    for (       ; col < endCol;  col++) putchar('#');
+    putchar('\n');
+  }
+}
+
+// Prints a triangle of specified height whose left edge is at col leftCol.
+void print_triangle(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('#');
+    putchar('\n');
+  }
+}
+
+void print_arrow(int leftCol, int size)
+{
+  print_triangle(leftCol, size);
+  print_square(leftCol, size);
+}
